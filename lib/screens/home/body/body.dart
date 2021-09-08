345f7aa623b8b92a928326f6d8b23b22ddff3689
Percycebo <percycebo@gmail.com>
2021-09-08 06:40:35 +0200

    Home Screen version 1

--- lib/screens/home/body/body.dart
+++ lib/screens/home/body/body.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_neat_and_clean_calendar/flutter_neat_and_clean_calendar.dart';
 class Body extends StatefulWidget {
 
   @override
@@ -9,29 +10,47 @@ class _BodyState extends State<Body> {
   Color color=Color.fromRGBO(245, 101, 136, 1);
   @override
   Widget build(BuildContext context) {
-    return Column(
-      children: [
-        Container(
-          height: 130,
-          color: color,
-          child: Center(
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.end,
-              children: [
-              Text('Work',style: TextStyle(
-                color: Colors.black,
-                fontSize: 20,
-                fontWeight: FontWeight.bold
-            ),
-            ),
-                SizedBox(
-                  height: 10,
-                )
-              ],
+    return SingleChildScrollView(
+      child: Column(
+        children: [
+          Container(
+            height: 130,
+            color: color,
+            child: Center(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.end,
+                children: [
+                Text('Work',style: TextStyle(
+                  color: Colors.black,
+                  fontSize: 20,
+                  fontWeight: FontWeight.bold
+              ),
+              ),
+                  SizedBox(
+                    height: 10,
+                  )
+                ],
+              ),
             ),
           ),
-        )
-      ],
+       SizedBox(
+         height: 100,
+         child: Calendar(
+                  startOnMonday: true,
+                  weekDays: ['Mon','Tue','Wed','Thur','Fri','Sat','Sun'],
+                  isExpandable: true,
+                  selectedColor: Colors.orange,
+                  todayColor: Colors.blue,
+                  locale: 'de_DE',
+                  todayButtonText: 'Namhlanje',
+                  isExpanded: true,
+                  expandableDateFormat:'EEEE, dd. MMMM yyyy',
+                  dayOfWeekStyle: TextStyle(
+                      color: Colors.black, fontWeight: FontWeight.w800, fontSize: 11
+                  ),
+                ),
+        ],
+      ),
     );
   }
 }
